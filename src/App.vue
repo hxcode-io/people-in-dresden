1a0af2445dd6723e5f94668614f6c6042dbc6f57
urbansky <urbansky@hxcode.io>
2021-07-14 16:01:57 +0200

    Parse json and show posts

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="container mx-auto bg-white h-full p-24">
     <h1>People in Dresden</h1>
-    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <post v-for="post in json" :key="post.timestamp"
+          :text="post.data[0].post"
+          :img="post.attachments[0].data[0].media.uri" class="m-16"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent, onMounted, ref} from 'vue'
+import Post from './components/Post.vue'
+import json from './posts_1.json';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Post
+  },
+  setup: () => {
+    onMounted(() => {
+      console.log('Component is mounted!', json)
+    })
+    return { json };
   }
 })
 </script>
