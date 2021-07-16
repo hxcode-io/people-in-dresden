9550040d98936dec06f66ce2316bef30b058a38d
Ahmad Moudallal <moudallal@hxcode.io>
2021-07-16 11:14:15 +0200

    check git

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto bg-white h-full p-24">
-    <h1 class="text-center">People in Dresden</h1>
+    <h1 class="text-center">People in Dresden hi</h1>
     <h2 class="text-center uppercase mb-24">And Their Stories</h2>
     <post-card v-for="post in posts" :key="post.text.de" :post="post" class="m-8 py-8 border-b-2"/>
   </div>
